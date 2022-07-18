873b89c1ede52e766d5e20d5ce15d69bd6edee7f
hcls-ga <98482497+hcls-ga@users.noreply.github.com>
2022-07-18 14:15:12 -0400

    Update merger1.py

--- merger1.py
+++ merger1.py
@@ -1,5 +1,5 @@
 from subprocess import Popen, PIPE
 from sys import executable
-p = Popen([executable, "/repositories/PassportQueue/manage.py makemigrations --merge"],stdin=PIPE)
+p = Popen([executable, "/home/n11unfbolhej/virtualenv/repositories/PassportQueue/manage.py makemigrations --merge"],stdin=PIPE)
 
 p.stdin.write('y'.encode())
